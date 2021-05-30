474325e262815eed8d41fc7ef1fb4ae6b58b2605
Renato <macaroots@gmail.com>
2021-05-29 22:28:23 -0300

    Add admin de pontos e CSS

--- app/rotas/iniciais.rotas.js
+++ app/rotas/iniciais.rotas.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const controle = require('../controle/index.controle');
 
-router.get('/', controle.index);
+router.get('/pessoas', controle.index);
 router.get('/admin/pessoas', controle.adminPessoas);
-router.get('/mapa', controle.mapa);
+router.get('/', controle.mapa);
 module.exports = router
