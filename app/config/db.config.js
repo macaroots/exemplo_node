a84795fd37ed249a3ad26e954a06b3d4694d8ded
Renato <macaroots@gmail.com>
2021-05-31 09:44:12 -0300

    BD mapa

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -5,7 +5,7 @@ const options = {
   host     : process.env.MYSQL_HOST || 'localhost',
   user     : process.env.MYSQL_USER || 'teste',
   password : process.env.MYSQL_PASSWORD || '123',
-  database : process.env.MYSQL_DATABASE || 'agenda'
+  database : process.env.MYSQL_DATABASE || 'mapa'
 };
 let fabricaMysql = new FabricaMysql(options);
 module.exports = fabricaMysql;
